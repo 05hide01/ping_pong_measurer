30f98d12d1bcb8125b5e9c2d0299a7c0a74f2f1e
05hide01 <7743572786@g.ecc.u-tokyo.ac.jp>
2025-05-15 18:50:10 +0900

    feature: switch protocol in shell script

--- src/ping_pong.sh
+++ src/ping_pong.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 payload_sizes=(1 10 100 1000 10000 100000 1000000)
 # payload_sizes=(10000000)
 pingpong_times=(0 1 10)
+protocols=(zenoh mqtt)
+date=$(date +"%Y-%m-%d")
 
-for pb in "${payload_sizes[@]}"; do
-    for pt in "${pingpong_times[@]}"; do
-        python3 process_measurement_helper.py --dn kakip_kakip_sh --pb $pb --pt $pt
+for protocol in "${protocols[@]}"; do
+    for pb in "${payload_sizes[@]}"; do
+        for pt in "${pingpong_times[@]}"; do
+            python3 $SCRIPT_DIR/process_measurement_helper.py --protocol $protocol --dn "$protocol/$date" --pb $pb --pt $pt
+        done
     done
 done
